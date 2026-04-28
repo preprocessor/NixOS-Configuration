2f77b51aa179df8f33b3fe34e15edfabd6025e28
wyspr <wyspr@wyspr.xyz>
2026-04-27 21:20:01 -0400

    new modules

--- modules/hosts/ramiel/modules.nix
+++ modules/hosts/ramiel/modules.nix
@@ -1,16 +1,25 @@
-{ inputs, self, ... }:
-let
-  inherit (self.modules) nixos;
-in
 {
-  flake.nixosConfigurations.ramiel = inputs.nixpkgs.lib.nixosSystem {
-    modules = with nixos; [
-      default
-      ramiel
-      desktop
-      tokyonight-night
-      shell
-    ];
-    system = "x86_64-linux";
-  };
+  inputs,
+  self,
+  withSystem,
+  ...
+}:
+{
+  flake.nixosConfigurations.ramiel = withSystem "x86_64-linux" (
+    { self', inputs', ... }:
+
+    inputs.nixpkgs.lib.nixosSystem {
+      specialArgs = {
+        inherit self' inputs';
+        inherit (inputs) wrappers;
+      };
+      modules = with self.modules.nixos; [
+        default
+        ramiel
+        desktop
+        tokyonight-night
+        shell
+      ];
+    }
+  );
 }
