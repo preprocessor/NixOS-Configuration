280089b43b09e9b4d37eb4594ede0e6836cf4c22
wyspr <wyspr@wyspr.xyz>
2026-05-15 10:28:53 -0400

    patch btop for /nix/store normalization
    from raf(NotAShelf)

---
+++ modules/aspects/shell/btop.nix
@@ -0,0 +1,21 @@
+{
+  perSystem =
+    { pkgs, ... }:
+    {
+      packages.btop = pkgs.btop-rocm.overrideAttrs {
+        patches = [
+          (pkgs.fetchpatch2 {
+            name = "normalize_processes";
+            url = "https://raw.githubusercontent.com/NotAShelf/nyxexprs/refs/heads/main/pkgs/btop/patches/normalize_processes.patch";
+            hash = "sha256-dh3TTb0Ix983W50inTzGflQ7mpBELaKReBUmzjBixTo=";
+          })
+        ];
+      };
+    };
+
+  w.default =
+    { self', ... }:
+    {
+      hj.packages = [ self'.packages.btop ];
+    };
+}
