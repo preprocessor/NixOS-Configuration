ef7ddeeff229af3fd2ebb1f5262f6b0413d2b45a
wyspr <wyspr@wyspr.xyz>
2026-06-22 20:27:41 -0400

    zathura hyprland float rule

--- aspects/modules/zathura/settings.nix
+++ aspects/modules/zathura/settings.nix
@@ -1,5 +1,15 @@
 {
   w.desktop = {
+    wrappers.hyprland.lua.files."window_rules".content = /* lua */ ''
+      hl.window_rule({
+        name = "float zathura",
+        match = {
+          class = "^org.pwmt.zathura$"
+        },
+        float = true,
+      })
+    '';
+
     wrappers.zathura = {
       enable = true;
     };
