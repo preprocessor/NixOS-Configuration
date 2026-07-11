94db049240dd975b604cda33259c121740b32e55
wyspr <wyspr@wyspr.xyz>
2026-07-11 18:21:03 -0400

    DAPARTED

--- flake.nix
+++ flake.nix
@@ -22,30 +22,91 @@
   outputs =
     { self, ... }:
     let
-      tackInputs = import ./.tack;
-
-      inherit (tackInputs.nixpkgs) lib;
-
-      inputs = tackInputs // {
-        self = self';
+      inputs = (import ./.tack) // {
+        inherit self;
       };
 
-      self' = flakeOutputs // {
-        inherit inputs;
-        inherit (self) outPath;
+      inherit (inputs.nixpkgs) lib;
+
+      rootPath = ./.;
+
+      projectInput =
+        system: input:
+        [
+          "packages"
+          "legacyPackages"
+          "devShells"
+          "checks"
+          "apps"
+          "formatter"
+        ]
+        |> lib.filter (key: input ? ${key} && input.${key} ? ${system})
+        |> lib.flip lib.genAttrs (key: input.${key}.${system});
+
+      withSystem =
+        system: f:
+        f {
+          inherit system inputs rootPath;
+          pkgs = inputs.nixpkgs.legacyPackages.${system};
+          inputs' = inputs |> lib.mapAttrs (_: projectInput system);
+          self' = projectInput system self;
+          packages' =
+            inputs
+            |> lib.mapAttrs (_: projectInput system)
+            |> lib.mapAttrs (
+              inputName: key:
+              if key.packages ? default then
+                key.packages // key.packages.default
+              else if key.packages ? "${inputName}" then
+                key.packages // key.packages."${inputName}"
+              else
+                key.packages
+            );
+        };
+
+      #  Evaluate the top-level modules
+      topEval = lib.evalModules {
+        specialArgs = { inherit inputs withSystem rootPath; };
+        modules =
+          with lib;
+          ./aspects
+          |> fileset.fileFilter (file: file.hasExt "nix" && !hasPrefix "_" file.name)
+          |> fileset.toList;
       };
 
-      flakeOutputs =
-        tackInputs.flake-parts.lib.evalFlakeModule { inherit inputs; } {
-          imports =
-            with lib;
-            ./aspects
-            |> fileset.fileFilter (file: file.hasExt "nix" && !hasPrefix "_" file.name)
-            |> fileset.toList;
-          _module.args.rootPath = ./.;
-        }
-        |> (eval: { inherit eval; } // eval.config.processedFlake);
+      # Evaluate omniSystem blocks for each system
+      # the output looks like this
+      # systemOutputs = { "x86_64-linux" = { packages = "pkg1"; devShells = "shell1"; }; }
+      systemOutputs =
+        topEval.config.systems
+        |> map (system: {
+          name = system;
+          value = withSystem system (
+            specialArgs:
+            (lib.evalModules {
+              inherit specialArgs;
+              modules = [
+                topEval.config.omniSystem
+                { config._module.freeformType = lib.types.lazyAttrsOf lib.types.unspecified; }
+              ];
+            }).config
+          );
+        })
+        |> lib.listToAttrs;
+
+      # Transpose system-dependent configurations to the top level
+      transposed =
+        systemOutputs
+        |> lib.attrValues
+        |> map lib.attrNames
+        |> lib.flatten
+        |> lib.unique
+        |> lib.flip lib.genAttrs (
+          key:
+          topEval.config.systems
+          |> lib.filter (system: systemOutputs.${system} ? ${key})
+          |> lib.flip lib.genAttrs (system: systemOutputs.${system}.${key})
+        );
     in
-    flakeOutputs;
-
+    topEval.config.flake // transposed;
 }
