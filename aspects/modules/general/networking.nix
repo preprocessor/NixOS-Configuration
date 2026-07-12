234693086dc48830ab9af12dd607f4af25b85345
wyspr <wyspr@wyspr.xyz>
2026-07-11 22:55:50 -0400

    add cake

--- aspects/modules/general/networking.nix
+++ aspects/modules/general/networking.nix
@@ -6,7 +6,7 @@
     boot = {
       kernelModules = [ "tcp_bbr" ];
       kernel.sysctl."net.ipv4.tcp_congestion_control" = "bbr";
-      kernel.sysctl."net.core.default_qdisc" = "fq"; # or "cake" for newer kernels
+      kernel.sysctl."net.core.default_qdisc" = "cake";
     };
   };
 }
