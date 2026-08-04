929d388cd5693949ac314f16d2d6db1ba78ed64d
wyspr <wyspr@wyspr.xyz>
2026-08-03 20:22:20 -0400

    add iris

---
+++ aspects/modules/shell/iris.nix
@@ -0,0 +1,17 @@
+{
+  tack.iris.url = "gh:versenilvis/iris/main";
+
+  exo.mods.desktop =
+    { packages', ... }:
+    {
+      hj.packages = [
+        packages'.iris
+      ];
+
+      programs.fish.interactiveShellInit = /* fish */ ''
+        if command -v iris >/dev/null 2>&1
+            alias i="iris"
+        end
+      '';
+    };
+}
