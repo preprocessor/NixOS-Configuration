b69f6986b95ff39ff25d450aac98e5e42daac8e0
wyspr <wyspr@wyspr.xyz>
2026-08-11 19:21:55 -0400

    add steam yazi keymap

--- aspects/modules/shell/yazi/keymaps.nix
+++ aspects/modules/shell/yazi/keymaps.nix
@@ -12,6 +12,10 @@
           (yaziKeymap [ "g" "m" ] "cd /run/media/wyspr/" "Go to Media")
           (yaziKeymap [ "g" "r" ] ''shell -- ya emit cd "$(git rev-parse --show-toplevel)"'' "Go to git root")
           (yaziKeymap [ "g" "n" ] "cd ${constants.cfgdir}" "Go to NixOS Configuration")
+          (yaziKeymap [
+            "g"
+            "s"
+          ] "cd ${config.hj.xdg.data.directory}/Steam/steamapps/common" "Go to Steam Apps")
 
           (yaziKeymap [ "H" ] "back" "Go to previous directory")
           (yaziKeymap [ "L" ] "forward" "Go to next directory")
