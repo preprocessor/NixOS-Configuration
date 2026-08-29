8dfbb5fd8e5dd9ca7cb307f866025406f29423da
wyspr <wyspr@wyspr.xyz>
2026-08-29 17:13:21 -0400

    sudo -> run0

--- aspects/modules/general/security.nix
+++ aspects/modules/general/security.nix
@@ -4,13 +4,10 @@
     {
       security = {
         sudo.enable = false;
-        sudo-rs = {
+        run0 = {
           enable = true;
-          wheelNeedsPassword = true;
-          execWheelOnly = true;
-          extraConfig = ''
-            Defaults pwfeedback
-          '';
+          sudo-shim.enable = true;
+          persistentAuth.enable = true;
         };
         # Whether to enable the RealtimeKit system service, which hands out realtime scheduling priority to user processes on demand. For example, PulseAudio and PipeWire use this to acquire realtime priority.
         rtkit.enable = true;
