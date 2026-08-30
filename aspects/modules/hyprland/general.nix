b49e622fb01672c2da3384213b12353a7f8db66c
wyspr <wyspr@wyspr.xyz>
2026-08-30 17:45:00 -0400

    cleanup

--- aspects/modules/hyprland/general.nix
+++ aspects/modules/hyprland/general.nix
@@ -1,67 +1,62 @@
 {
-  exo.mods.desktop =
-    { packages', ... }:
-    {
-      my.hyprland = {
-        enable = true;
-        package = packages'.hyprland;
-      };
+  exo.mods.desktop = {
+    my.hyprland.enable = true;
 
-      my.hyprland.lua.files."general".content = /* lua */ ''
-        hl.monitor({
-          output = "DP-2",
-          mode = "3440x1440@74.983",
-          position = "0x0",
-          scale = 1,
-        })
+    my.hyprland.lua.files."general".content = /* lua */ ''
+      hl.monitor({
+        output = "DP-2",
+        mode = "3440x1440@74.983",
+        position = "0x0",
+        scale = 1,
+      })
 
-        hl.config({
-          render = {
-            direct_scanout = 2,
-          },
+      hl.config({
+        render = {
+          direct_scanout = 2,
+        },
 
-          cursor = {
-            inactive_timeout = 5,
-            hide_on_key_press = true,
-            zoom_disable_aa = true,
-            no_warps = true,
-            warp_back_after_non_mouse_input = false,
-            warp_on_change_workspace = 2,
-            warp_on_toggle_special = 2,
-          },
+        cursor = {
+          inactive_timeout = 5,
+          hide_on_key_press = true,
+          zoom_disable_aa = true,
+          no_warps = true,
+          warp_back_after_non_mouse_input = false,
+          warp_on_change_workspace = 2,
+          warp_on_toggle_special = 2,
+        },
 
-          binds = {
-            workspace_back_and_forth = true,
-          },
+        binds = {
+          workspace_back_and_forth = true,
+        },
 
-          misc = {
-            force_default_wallpaper = 0,    -- Set to 0 or 1 to disable the anime mascot wallpapers
-            disable_hyprland_logo   = true, -- If true disables the random hyprland logo / anime girl background. :(
-            focus_on_activate       = true,
-            middle_click_paste      = false,
-          },
+        misc = {
+          force_default_wallpaper = 0,    -- Set to 0 or 1 to disable the anime mascot wallpapers
+          disable_hyprland_logo   = true, -- If true disables the random hyprland logo / anime girl background. :(
+          focus_on_activate       = true,
+          middle_click_paste      = false,
+        },
 
-          input = {
-            kb_layout          = "us",
-            kb_options         = "caps:escape",
+        input = {
+          kb_layout          = "us",
+          kb_options         = "caps:escape",
 
-            numlock_by_default = true,
+          numlock_by_default = true,
 
-            repeat_delay       = 300,
-            repeat_rate        = 40,
+          repeat_delay       = 300,
+          repeat_rate        = 40,
 
-            follow_mouse       = 2,
-            mouse_refocus       = false,
+          follow_mouse       = 2,
+          mouse_refocus       = false,
 
-            focus_on_close      = 2,
-            float_switch_override_focus = 0,
-          },
-        })
+          focus_on_close      = 2,
+          float_switch_override_focus = 0,
+        },
+      })
 
-        -- Restore wallpaper on monitor reconnect
-        hl.on("monitor.added", function()
-          hl.dispatch(hl.dsp.exec_raw("waypaper --restore"))
-        end)
-      '';
-    };
+      -- Restore wallpaper on monitor reconnect
+      hl.on("monitor.added", function()
+        hl.dispatch(hl.dsp.exec_raw("waypaper --restore"))
+      end)
+    '';
+  };
 }
