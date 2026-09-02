79331c8d29482957bbe25d577ce8f78eb7f759f4
wyspr <wyspr@wyspr.xyz>
2026-09-02 13:00:20 -0400

    combine scrindle settings

--- aspects/modules/hyprland/layouts/scrindle.nix
+++ aspects/modules/hyprland/layouts/scrindle.nix
@@ -1,6 +1,21 @@
 {
   exo.mods.desktop = {
     my.hyprland.lua.files."layouts.scrindle".content = /* lua */ ''
+      hl.config({
+        layout = {
+          single_window_aspect_ratio = { 16, 9 },
+        },
+
+        scrolling = {
+          fullscreen_on_one_column = false,
+          explicit_column_widths = "0.25, 0.33333, 0.5, 0.66667, 0.75",
+          -- column_width = 0.7296,
+          column_width = 0.33333,
+          wrap_focus = false,
+          wrap_swapcol = false,
+        },
+      })
+
       local sort_windows_ltr = function(windows)
         table.sort(windows, function(a, b)
           return a.at.x < b.at.x
