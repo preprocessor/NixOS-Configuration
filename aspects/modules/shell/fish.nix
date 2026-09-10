4217fb4c0f8cba596ee84b7867a4440d88c68e7e
wyspr <wyspr@wyspr.xyz>
2026-09-10 16:19:58 -0400

    remove custom fish function option for the nixos module

--- aspects/modules/shell/fish.nix
+++ aspects/modules/shell/fish.nix
@@ -4,61 +4,6 @@
     type = "fetch";
   };
 
-  exo.skeleton =
-    {
-      config,
-      pkgs,
-      lib,
-      ...
-    }:
-    {
-      options.programs.fish.shellFunctions = lib.mkOption {
-        type = lib.types.attrsOf (
-          lib.types.submodule {
-            options = {
-              body = lib.mkOption {
-                type = lib.types.str;
-              };
-
-              modifiers = lib.mkOption {
-                type = lib.types.str;
-                default = "";
-              };
-            };
-
-          }
-        );
-      };
-
-      config =
-        let
-          cfg = config.programs.fish;
-
-          indentFishFile =
-            name: text:
-            pkgs.runCommandLocal name {
-              nativeBuildInputs = [ cfg.package ];
-              inherit text;
-              passAsFile = [ "text" ];
-            } "fish --no-config -c 'fish_indent $textPath' > $out";
-        in
-        {
-          environment.etc = lib.mapAttrs' (name: value: {
-            name = "fish/functions/${name}.fish";
-            value.source = indentFishFile "${name}.fish" ''
-              function ${name} ${value.modifiers}
-                ${lib.trim value.body}
-              end
-            '';
-          }) cfg.shellFunctions;
-
-          programs.fish.shellInit = ''
-            # Add the search path for global fish functions
-            set fish_function_path /etc/fish/functions/ $fish_function_path
-          '';
-        };
-    };
-
   exo.core =
     {
       inputs,
@@ -74,7 +19,6 @@
           fish_vi_key_bindings # Vim mode
 
           set -g fish_greeting # Disable greeting
-
           set -g SHELL_PROGRAM fish
 
           # setup fish-completion-sync
@@ -83,7 +27,6 @@
           bind Z __onelockeds_fuzzy_zox
           bind -M insert Z __onelockeds_fuzzy_zox
         '';
-
       };
     };
 }
