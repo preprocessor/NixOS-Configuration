70bc15eb8276e25df2985eaf66ea150cc89bcf23
wyspr <wyspr@wyspr.xyz>
2026-09-20 19:55:04 -0400

    neovim: highlight-colors upgrade

---
+++ aspects/modules/programs/tui/neovim/plugins/highlight-colors.nix
@@ -0,0 +1,11 @@
+{
+  exo.mods.neovim = {
+    plugins.highlight-colors = {
+      enable = true;
+      settings = {
+        render = "virtual";
+        enable_named_colors = false;
+      };
+    };
+  };
+}
--- aspects/modules/programs/tui/neovim/plugins/plugins.nix
+++ aspects/modules/programs/tui/neovim/plugins/plugins.nix
@@ -3,7 +3,6 @@
     plugins = {
       ts-comments.enable = true;
       ts-context-commentstring.enable = true;
-      highlight-colors.enable = true;
       lz-n.enable = true;
       mini = {
         enable = true;
